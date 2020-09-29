6765a1a83db45ad64c1b55b6e5dd3521a96847f0
Afonso Machado <afonso777@hotmail.com>
2020-09-29 20:40:13 -0300

    finalizado e definindo TODOs

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,11 @@ import GlobalStyles from "./styles/GlobalStyles";
 function App() {
   return (
     <>
+      {/* TODO Tornar a interface responsiva */}
+      {/* TODO Visualizar item com zoom */}
+      {/* TODO Slider na paete de baixo */}
+      {/* TODO Perguntas e respostas / estrelas */}
+
       <Layout />
       <GlobalStyles />
     </>
--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import Footer from "../Footer";
+import Header from "../Header";
 import Product from "../Product";
 
 import { Container, Wrapper } from "./styles";
@@ -6,13 +8,13 @@ import { Container, Wrapper } from "./styles";
 const Layout: React.FC = () => {
   return (
     <Container>
-      {/* <Header /> */}
+      <Header />
 
       <Wrapper>
         <Product />
       </Wrapper>
 
-      {/* <Footer /> */}
+      <Footer />
     </Container>
   );
 };
