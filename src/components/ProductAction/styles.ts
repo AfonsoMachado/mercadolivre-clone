417eaa2661ebd0879f699f09436e585b1c028143
Afonso Machado <afonso777@hotmail.com>
2020-09-29 15:46:29 -0300

    estrutura basica do ProductAction

---
+++ src/components/ProductAction/styles.ts
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+import {
+  HiOutlineHeart,
+  HiOutlineCheck,
+  HiOutlineShieldCheck,
+} from "react-icons/hi";
+
+export const Container = styled.div``;
+
+export const Condition = styled.div``;
+
+export const Row = styled.div``;
+
+export const HeartIcon = styled(HiOutlineHeart)``;
+
+export const DispatchTag = styled.div``;
+
+export const PriceCard = styled.div``;
+
+export const PriceRow = styled.div``;
+
+export const InstallmentsInfo = styled.div``;
+
+export const StockStatus = styled.div``;
+
+export const MethodCard = styled.div``;
+
+export const CheckIcon = styled(HiOutlineCheck)``;
+
+export const Actions = styled.div``;
+
+export const Button = styled.div``;
+
+export const Benefits = styled.div``;
+
+export const ShieldIcon = styled(HiOutlineShieldCheck)``;
